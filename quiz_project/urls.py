54fd9207dc1cd03fa07b81b00e7a238a655324f9
zinosalaam1 <bolatitosalaam7@gmail.com>
2026-02-25 22:14:33 +0100

    backend changes

--- quiz_project/urls.py
+++ quiz_project/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from quiz_api import views
@@ -25,7 +26,12 @@ router.register(r'questions', views.QuestionViewSet)
 router.register(r'sessions', views.GameSessionViewSet)
 router.register(r'answers', views.AnswerViewSet)
 
+
+def home(request):
+    return HttpResponse("Quiz Server is Live 🚀")
+
 urlpatterns = [
+    path("", home), 
     path('admin/', admin.site.urls),
     path("api/", include("quiz_api.urls")),
     path('api/', include(router.urls)),
