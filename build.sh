2f3e45423c24f7947e930debd0bd54fb7c27afe3
zinosalaam1 <bolatitosalaam7@gmail.com>
2026-02-26 08:33:06 +0100

    backend changes

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 
 pip install -r requirements.txt
 
-python manage.py migrate quiz_api --fake
+python manage.py makemigrations quiz_api
+python manage.py migrate quiz_api 
 python manage.py migrate
 
 python manage.py collectstatic --noinput
