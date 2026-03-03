e4d455c0c58f6448173ad34ef36c1763d1490930
zinosalaam1 <bolatitosalaam7@gmail.com>
2026-03-03 16:57:29 +0100

    Set Python version to 3.12

--- quiz_api/urls.py
+++ quiz_api/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from . import views
 from .views import get_active_session
+from rest_framework.routers import DefaultRouter
+from .views import TeamViewSet
+
+router = DefaultRouter()
+router.register(r"teams", TeamViewSet, basename="team")
 
 urlpatterns = [
     path("auth/admin-login/", views.admin_login),
     path("active-session/", get_active_session),
-    path("teams/", views.TeamViewSet.as_view()),
     path("session/active/", views.ActiveSessionView.as_view()),
 ]
+
+urlpatterns += router.urls
